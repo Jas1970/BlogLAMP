577710e3f51b3fd1f71c6b0e9927b3aae6884835
Jaswinder Singh <haanzee@gmail.com>
2020-07-27 20:56:03 +0530

    First Commit Residence Repo

---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> BlogLAMP </title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/Bootstrap.min.css'); ?>">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark bg-primary nav-fill w-80">
+  <a class="navbar-brand" href="#">Navbar</a>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url('home'); ?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url('about'); ?>">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>
