02d329a41445ca0942d033c184f17ffafaabd8a3
Jaswinder Singh <haanzee@gmail.com>
2020-08-01 08:23:26 +0530

    Add Category Functionality to Blog

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -2,8 +2,7 @@
 <?php echo validation_errors(); ?>
 
 
-<?php echo form_open('posts/create'); ?>
-  </div>
+<?php echo form_open_multipart('posts/create'); ?>
   <div class="form-group">
     <label>Title</label>
     <Input type="text" class="form-control" name="title" placeholder="Add Title"></textarea>
@@ -12,5 +11,25 @@
     <label>Body</label>
     <textarea id="editor1" class="form-control" name="body" placeholder="Add Body"></textarea>
   </div>
+<div class="form-group">
+	  <label>Category</label>
+	  <select name="category_id" class="form-control">
+		  <?php foreach($categories as $category): ?>
+		  	<option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+		  <?php endforeach; ?>
+	  </select>
+  </div>
+  <div class="form-group">
+  	<label>Upload Image</label>
+  	<input type="file" name="userfile" size="20">
+
+  </div>
+
+
+
+
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+
--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,13 +1,12 @@
 <h2><?php echo $post['title']; ?> </h2>
-<small class="post-date">Posted on : <?php echo $post['created_at']; ?> </small><br>
-<div class="post-body">
-<?php echo $post['body']; ?> 
-</div>
-
-<hr>
-<?php echo form_open('/posts/delete/'.$post['id']); ?>
-	<input type="submit" value="Delete" class="btn btn-danger">
-	<a class="btn btn-primary pull-left" href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
-
-</form> 	
+	<small class="post-date">Posted on : <?php echo $post['created_at']; ?> </small><br><br>
+	<img class="img-thumbnail" src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image']; ?>">	
+		<div class="post-body">
+			<?php echo $post['body']; ?> 
+		</div>
+		<hr>
+	<?php echo form_open('/posts/delete/'.$post['id']); ?>
+		<input type="submit" value="Delete" class="btn btn-danger"></input>
+		<a class="btn btn-primary pull-left" href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
+	</form> 	
 
